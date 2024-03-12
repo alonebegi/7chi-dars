ac90526ac11232ec18d991ce91d37c95116039ca
Sunnatillo Azimjonov <monomusix001@gmail.com>
2024-03-12 15:49:37 +0300

    masiv

---
+++ app.js
@@ -0,0 +1,52 @@
+// // 73
+
+// function hajm(quti) {
+//   let height = +prompt("balandlikni kriting:");
+//   let width = +prompt("eni kriting:");
+//   let length = +prompt("uzunlikni kriting:");
+//   let hajmi = height * width * length;
+//   quti = hajmi;
+//   console.log(hajmi);
+//   return hajmi;
+// }
+// hajm();
+
+// 83masala
+// function word(worlds, works) {
+//   worlds = prompt("so'z yozing: ");
+//   works = prompt("randm soni kriting");
+//   worlds = toString;
+//   works = toString();
+//   if ((worlds = works)) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// word();
+
+// 84masala
+// function uzunlik(lengt) {
+//   lengt = prompt("soz kriting");
+//   console.log(lengt.length);
+//   return lengt;
+// }
+// uzunlik();
+
+// 86masala
+// function kvadrat(one, two, three) {
+//   one = +prompt("brnchi son kriting: ");
+//   two = +prompt("ikkinchi son kriting: ");
+//   three = +prompt("uchunchi son kriting: ");
+//   console.log(one * 2, two * 2, three * 2);
+// }
+// kvadrat();
+
+// 97
+// function namuna() {
+//   let arr;
+//   arr = [2, 5, 6];
+//   console.log(arr);
+
+// }
+// namuna();
